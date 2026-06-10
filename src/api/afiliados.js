abfe3acf7990dab71615d05e11e8405025176266
Camila Guarnieri <camiiguarnieri@gmail.com>
2026-06-10 10:07:27 -0300

    ruta de busqueda afiiado

--- src/api/afiliados.js
+++ src/api/afiliados.js
@@ -4,7 +4,6 @@ export const getAfiliados = (page = 1, limit = 20, search = '') =>
   client.get('/afiliado', { params: { page, limit, ...(search && { search }) } });
 
 export const createAfiliado = (data) => client.post('/afiliado', data);
-
 export const updateAfiliado = (id, data) => client.put(`/afiliado/${id}`, data);
-
 export const deleteAfiliado = (id) => client.delete(`/afiliado/${id}`);
+export const searchAfiliados = (q) => client.get('/afiliados/search', { params: { q } });
